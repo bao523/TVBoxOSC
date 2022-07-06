9a84c4a7ba0f552cc88393a94b6fa2c1754ff3d4
bao523 <107175738+bao523@users.noreply.github.com>
2022-07-06 08:23:13 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitea.com/bao523/bao/raw/branch/main/bao.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
